863a35b83ddaa66f0a3180e20b29767af52afa7e
Guilherme Cardoso <guilherme-cardoso@msn.com>
2011-06-08 03:17:53 +0100

    Código

---
+++ source/QUnitTestSample/Scripts/AlunosTest.js
@@ -0,0 +1,12 @@
+﻿function Deve_Validar_Codigo(codigoAluno) {
+    var resultado = new OutputJS();
+    if (codigoAluno.length <= 4) {
+        resultado.Codigo = false;
+        resultado.Mensagem = "O código de aluno deverá ter no mínimo 4 caracteres.";
+    }
+    else {
+        resultado.Codigo = true;
+        resultado.Mensagem = "Código de aluno validado com sucesso";
+    }
+    return resultado;
+}
